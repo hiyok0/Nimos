f7403e1654c54fc0a13ae156cbcff1180081dc26
hiyok0 <qwertykeyboardtwitcasting+git@gmail.com>
2022-05-12 04:09:02 +0900

    関数をまとめた。一個ずつ再生するのがなくなった。
    再生のキュー管理は後で戻す。

--- index.js
+++ index.js
@@ -1,106 +1,104 @@
-//variable
-let playcmd = "mpv -";
-let voicevoxSettings = {
-	"address": "localhost",
-	"port"   : "50021",
-	"speaker": "14"		//配列にして複数指定できても面白いかもしれない
-};
-let playQueue = [];
-let playLock = false;
-
-//require
+//import
 import fetch from "node-fetch";
 import express from "express";
-//import fs from 'fs';
 import childProcess from 'child_process';
 import p from 'process';
 
-//テスト用クソコード、考えるのがめんどくさかったからコピペしてる。
-//ちゃんと出来たら多分消す
-switch (p.argv.length){
-	case 6:
-		voicevoxSettings.speaker = p.argv[5];
-		voicevoxSettings.port = p.argv[4];
-		voicevoxSettings.address = p.argv[3];
-	case 5:
-		voicevoxSettings.port = p.argv[4];
-		voicevoxSettings.address = p.argv[3];
-	case 4:
-		voicevoxSettings.address = p.argv[3];
-}
-
-//setup
-//port
-const listenPort = setListenPort(p.argv[2]);	//まとめる書き方ありそう
-function setListenPort(port){
+//express
+const listenPort = setListenPort(p.argv[2]);	//なんかまとめれない。　なんでぇ……？？？
+function setListenPort(port){					//SyntaxError: Unexpected token '.'
 	switch (isNaN(port)) {
 		case false:
 			if ( port >= 32768|| 
 				 port == 8008 || 	//https://ja.wikipedia.org/wiki/TCPやUDPにおけるポート番号の一覧
 				 port == 8080 || 	//上記に載っているHTTP Alternateなユーザポートと
-				 port == 8000 ||	//私がたまーに見かける8000番、
-				 port == 8081		//動的・私用ポートを指定している。
+				 port == 8000 ||	//私がたまーに見かける8000番、動的・私用ポートを指定している。
+				 port == 8081		//3000番とかもあったほうが良いかな？
 			) {
 				return port;
 				break;
 			}else{
-				console.log("u must set 8000,8008,8080,8081 or over 32767 as port number.\ndefault port is 50080");
+				console.log("u must set (8000,)8008,8080,8081 or over 32767 as port number.\ndefault port is 50080");
 			}
 		default:
 			if (isNaN(port)){console.log("Not a Number");}
 			return 50080;
 	}
 }
-//express
-var app = express();
-var server = app.listen(listenPort, function(){
-	console.log("nusuttoChan is listening to PORT:" + server.address().port);
+
+//const listenPort = 50080
+let app = express();
+let server = app.listen(listenPort, function(){
+	console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
+
 //VOICEVOX
-async function voicevoxProcess(textEnc){
-	//query
-	console.log(textEnc);
-	let queryObj  = await fetch("http://"+voicevoxSettings.address+":"+voicevoxSettings.port+"/audio_query?text="+textEnc+"&speaker="+voicevoxSettings.speaker,{method: 'POST'});
-	let queryJson = await queryObj.json();
-	console.log(queryJson);
-	//synthesis
-	let onsei = await fetch("http://"+voicevoxSettings.address+":"+voicevoxSettings.port+"/synthesis?speaker="+voicevoxSettings.speaker,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-				},
-			body: JSON.stringify(queryJson)
+let voicevox = {
+	"settings": {				//voicevoxの設定は全部ここに
+		"address": "localhost",
+		"port"   : "50021",
+		"speaker": "14"
+	},
+	"query": {
+		"process": async function(text){
+			let queryObj  = await fetch("http://"+voicevox.settings.address+":"+voicevox.settings.port+"/audio_query?text="+encodeURIComponent(text)+"&speaker="+voicevox.settings.speaker,{method: 'POST'});
+			let queryJson = await queryObj.json();
+			console.log(queryJson);
+			return queryJson;
 			}
-	);
-	return await onsei.arrayBuffer();
+	},
+	"synthesis": {
+		"queues":[],
+		"lock": 1,
+		"process": async function(query) {
+			let onsei = await fetch("http://"+voicevox.settings.address+":"+voicevox.settings.port+"/synthesis?speaker="+voicevox.settings.speaker,
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+						},
+					body: JSON.stringify(query)
+					}
+			);
+			return await onsei.arrayBuffer();
+		}
+	}
+}
+//テスト用クソコード、考えるのがめんどくさかったからコピペしてる。
+//ちゃんと出来たら多分消す
+switch (p.argv.length){
+	case 6:
+		voicevox.settings.speaker = p.argv[5];
+		voicevox.settings.port = p.argv[4];
+		voicevox.settings.address = p.argv[3];
+	case 5:
+		voicevox.settings.port = p.argv[4];
+		voicevox.settings.address = p.argv[3];
+	case 4:
+		voicevox.settings.address = p.argv[3];
 }
 
+
+//playing
+let playing = {
+	"command": "mpv -",
+	"queues":[],
+	"lock": false,
+	"main": async function(onseiArrayBuffer){
+		let saisei = childProcess.exec(playing.command, function(err, result) {
+			if (err) return console.log(err);
+			console.log(result);
+		});
+		await saisei.stdin.write(new Uint8Array(onseiArrayBuffer));
+		await saisei.stdin.end();
+	}
+}
 //machi-uke 
 app.get("/talk", async function(req) {
 	console.log(req.query.text);
-	let onseiArrayBuffer = await voicevoxProcess(encodeURIComponent(req.query.text));
+	let queryJson = await voicevox.query.process(req.query.text);
+	let onseiArrayBuffer = await voicevox.synthesis.process(queryJson);
+	playing.main(onseiArrayBuffer);
 	
-	
-	//await fs.writeFileSync( "./audio.wav" , new Uint8Array(onseiArrayBuffer), 'binary');
-
-	await playQueue.push(new Uint8Array(onseiArrayBuffer));
 });
-
-function playSound () {
-	while (playQueue.length && !playLock){		//聖徳太子モード考えるとswitchのほうが良かったかも
-		playLock = true;
-		console.log("playQueue.length: "+playQueue.length);
-		let mpv = childProcess.exec(playcmd, function(err, result) {	//本当はspawnのほうが良いのかな？
-			if (err) return console.log(err);
-			console.log(result);
-			playLock = false;
-		});
-		mpv.stdin.write(playQueue.shift());
-		mpv.stdin.end();
-	}
-}
-
-setInterval(playSound,500);
-
